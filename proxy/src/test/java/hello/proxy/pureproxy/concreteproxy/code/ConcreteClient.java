20b26d1c476f75b57d0970c2e898185d1b4a3186
rugii913 <rugii913@gmail.com>
2023-06-20 15:52:02 +0900

    구체 클래스 기반 프록시 예제2 - 클래스 기반 프록시 도입
    *** 자바 언어에서 다형성은 인터페이스나 클래스를 구분하지 않고 모두
    적용된다. 해당 타입과 그 타입의 하위 타입은 모두 다형성의 대상이
    된다.(상위 타입만 맞는다면)
    
    ㅁTimeProxy extends ConcreteLogic - 시간 측정 부가 기능 제공
     ㅇ인터페이스가 아닌 ConcreteLogic 클래스를 상속받아서 프록시 구현 가능
    
    ㅁConcreteProxyTest - addProxy()
     ㅇ핵심: ConcreteClient의 생성자에 concreteLogic이 아닌 timeProxy를 주À
     ㅇ런타임 객체 의존 관계 조립: client -> timeProxy -> concreteLogic

--- proxy/src/test/java/hello/proxy/pureproxy/concreteproxy/code/ConcreteClient.java
+++ proxy/src/test/java/hello/proxy/pureproxy/concreteproxy/code/ConcreteClient.java
@@ -2,7 +2,8 @@ package hello.proxy.pureproxy.concreteproxy.code;
 
 public class ConcreteClient {
 
-    private ConcreteLogic concreteLogic;
+    private ConcreteLogic concreteLogic; //ConcreteLogic, TimeProxy 모두 주입 가능
+    // - 클라이언트는 ConcreteLogic이 들어왔는지, TimeProxy가 들어왔는지 모른다.
 
     public ConcreteClient(ConcreteLogic concreteLogic) {
         this.concreteLogic = concreteLogic;
