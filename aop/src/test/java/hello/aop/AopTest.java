d96bb52496369089a998902c6ca1bad7777530ff
rugii913 <rugii913@gmail.com>
2023-06-26 13:05:46 +0900

    스프링 AOP 구현5 - 어드바이스 순서
    ㅁ@Order 어노테이션을 적용
     ㅇ하지만 @Aspect가 적용 단위이기 때문에 순서가 꼭 보장되어야한다면 각
     aspect를 별도의 클래스로 분리해야한다.

--- aop/src/test/java/hello/aop/AopTest.java
+++ aop/src/test/java/hello/aop/AopTest.java
@@ -2,10 +2,7 @@ package hello.aop;
 
 import hello.aop.order.OrderRepository;
 import hello.aop.order.OrderService;
-import hello.aop.order.aop.AspectV1;
-import hello.aop.order.aop.AspectV2;
-import hello.aop.order.aop.AspectV3;
-import hello.aop.order.aop.AspectV4Pointcut;
+import hello.aop.order.aop.*;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -19,7 +16,8 @@ import org.springframework.context.annotation.Import;
 //빈 바꾸면서 등록할 때 편하게 쓸 수 있음
 //@Import(AspectV2.class)
 //@Import(AspectV3.class)
-@Import(AspectV4Pointcut.class)
+//@Import(AspectV4Pointcut.class)
+@Import({AspectV5Order.LogAspect.class, AspectV5Order.TxAspect.class})
 @SpringBootTest
 public class AopTest {
 
