e6c8505779245beab7b3a7e33ff8ad7ab8f723b7
rugii913 <rugii913@gmail.com>
2023-06-26 12:54:24 +0900

    스프링 AOP 구현4 - 포인트컷 참조
    ㅁ공용 포인트컷 별도로 패키지 분리
     ㅇ접근 제어자는 public이어야 함
     ㅇ포인트컷 조합 가능
     ㅇ포인트컷 시그니처 적어줄 때 패키지 이름, 클래스 이름까지 적어줘야함(intelliJ는
     자동 완성 도와줌)

--- aop/src/test/java/hello/aop/AopTest.java
+++ aop/src/test/java/hello/aop/AopTest.java
@@ -5,6 +5,7 @@ import hello.aop.order.OrderService;
 import hello.aop.order.aop.AspectV1;
 import hello.aop.order.aop.AspectV2;
 import hello.aop.order.aop.AspectV3;
+import hello.aop.order.aop.AspectV4Pointcut;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -17,7 +18,8 @@ import org.springframework.context.annotation.Import;
 //@Import(AspectV1.class) //cf. p.277 참고, 주로 설정 파일 추가할 때 사용하지만, 바로 스프링 빈 등록할 수도 있다.
 //빈 바꾸면서 등록할 때 편하게 쓸 수 있음
 //@Import(AspectV2.class)
-@Import(AspectV3.class)
+//@Import(AspectV3.class)
+@Import(AspectV4Pointcut.class)
 @SpringBootTest
 public class AopTest {
 
