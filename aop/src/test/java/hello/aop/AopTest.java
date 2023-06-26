ed66a84a9c31b8152586f04d8a116344354c18b2
rugii913 <rugii913@gmail.com>
2023-06-26 12:29:29 +0900

    스프링 AOP 구현3 - 어드바이스 추가
    ㅁ포인트컷 allService() 추가, 어드바이스 doTransaction() 추가
     ㅇ여러 포인트컷과 여러 어드바이스를 적용할 수 있다.
     ㅇ그러면 어드바이스 적용 순서는?? -> 다다음 강의에서

--- aop/src/test/java/hello/aop/AopTest.java
+++ aop/src/test/java/hello/aop/AopTest.java
@@ -4,6 +4,7 @@ import hello.aop.order.OrderRepository;
 import hello.aop.order.OrderService;
 import hello.aop.order.aop.AspectV1;
 import hello.aop.order.aop.AspectV2;
+import hello.aop.order.aop.AspectV3;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -15,7 +16,8 @@ import org.springframework.context.annotation.Import;
 @Slf4j
 //@Import(AspectV1.class) //cf. p.277 참고, 주로 설정 파일 추가할 때 사용하지만, 바로 스프링 빈 등록할 수도 있다.
 //빈 바꾸면서 등록할 때 편하게 쓸 수 있음
-@Import(AspectV2.class)
+//@Import(AspectV2.class)
+@Import(AspectV3.class)
 @SpringBootTest
 public class AopTest {
 
